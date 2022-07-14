e5b18fb09c9aabc83ee1915695972701eba75587
estherjanesamuel <esther.jane.samuel@gmail.com>
2022-07-14 10:50:27 +0700

    add grpc and mvc

---
+++ src/api-clients-communications/grpc/GrpcGreeter/Program.cs
@@ -0,0 +1,23 @@
+using Server.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddGrpcHttpApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+// For static content
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapGrpcService<GreeterService>();
+app.Run();
---
+++ src/api-clients-communications/grpc/GrpcGreeterClient/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GrpcGreaterClient;
+
+Console.WriteLine("Hello, World!");
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress(" http://localhost:5256");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+    new HelloRequest { Name = "Arief" }
+);
+
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
