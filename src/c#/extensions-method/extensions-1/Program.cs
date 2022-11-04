954307d350e005fb713604bdce15edf99f3a2039
estherjanesamuel <esther.jane.samuel@gmail.com>
2022-11-04 14:59:53 +0700

    update c#, extensions

--- src/c#/extensions-method/extensions-1/Program.cs
+++ src/c#/extensions-method/extensions-1/Program.cs
@@ -2,6 +2,7 @@
 GoatUtils.AddGoadSkill(goat, "dotnet");
 var level = GoatUtils.GetGoatLevel(goat);
 Console.WriteLine("Goat level: {0}", level);
+Console.WriteLine("Goat skills: [{0}]", string.Join(", ", goat.Skills));
 
 goat.AddSkill("c#");
 goat.AddSkill("blazor");
@@ -11,10 +12,18 @@ goat.AddSkill("linq");
 Console.WriteLine("Goat skills: [{0}]", string.Join(", ", goat.Skills));
 Console.WriteLine("Goat level: {0}", goat.CurrentLevel());
 
+goat.AddSkill("minimal api");
+goat.AddSkill("mvc");
+goat.AddSkill("iis");
+goat.AddSkill("identity server");
+Console.WriteLine("Goat skills: [{0}]", string.Join(", ", goat.Skills));
+Console.WriteLine("Goat level: {0}", goat.CurrentLevel());
+Console.WriteLine("Goat level: {0}", GoatUtils.GetGoatLevelByYearsExperience(new DateTime(2019,11,04)));
 
 public record class Goat
 {
     public List<string> Skills {get; set;} = new();
+    public int YearsExperience { get; set; } = 0;
 }
 public enum GoatLevel
 {
@@ -52,6 +61,27 @@ public static class GoatUtils
         if (!goat.Skills.Contains(skill))
         {
             goat.Skills.Add(skill);
+        }
+    }
+
+    public static GoatLevel GetGoatLevelByYearsExperience(DateTime startDate)
+    {
+        /*
+            1 year is 12 month 
+            how to compare now with the start date
+        */
+        var totalDays = DateTime.Today.Subtract(startDate).TotalDays;
+        var daysToSenior = 365 * 3 - totalDays;
+        Console.WriteLine("Need days to be a senior: {0} ", daysToSenior);
+        switch (totalDays)
+        {
+            case < 365 * 3:
+            return GoatLevel.Junior;
+            case < 365 * 5:
+            
+            return GoatLevel.Senior;
+            default:
+            return GoatLevel.God;
         }
     }
 
