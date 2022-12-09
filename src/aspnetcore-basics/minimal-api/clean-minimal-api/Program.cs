f697171b7bc904481bf85214eb7f6249eccfc156
estherjanesamuel <esther.jane.samuel@gmail.com>
2022-12-09 17:05:49 +0700

    update c#, asp net basics

--- src/aspnetcore-basics/minimal-api/clean-minimal-api/Program.cs
+++ src/aspnetcore-basics/minimal-api/clean-minimal-api/Program.cs
@@ -1,10 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
-var config = builder.Configuration;
-
-builder.Services.AddHealthChecks();
-
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapHealthChecks("/healtz");
+
 app.Run();
